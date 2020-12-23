d3318600e4bdfe54c57982723ad9f772cfaca890
AparisBlink <61824533+AparisBlink@users.noreply.github.com>
2020-12-23 13:47:49 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,117 @@
+(function (blink) {
+	'use strict';
+
+	var smchile2021Style = function () {
+			blink.theme.styles.basic.apply(this, arguments);
+		},
+		page = blink.currentPage;
+
+	smchile2021Style.prototype = {
+		//BK-15873 añadimos el estilo basic como parent para la herencia de los estilos del CKEditor
+		parent: blink.theme.styles.basic.prototype,
+		bodyClassName: 'content_type_clase_smchile2021',
+		ckEditorStyles: {
+			name: 'smchile2021',
+			styles: [
+
+				{ name: 'Título 1', element: 'h4', attributes: { 'class': 'bck-title bck-title1'} },
+				{ name: 'Título 2', element: 'h4', attributes: { 'class': 'bck-title bck-title2'} },
+				{ name: 'Título 3', element: 'h4', attributes: { 'class': 'bck-title bck-title3'} },
+				{ name: 'Título 4', element: 'h4', attributes: { 'class': 'bck-title bck-title4'} },
+				{ name: 'Título 5', element: 'h4', attributes: { 'class': 'bck-title bck-title5'} },
+				{ name: 'Título 6', element: 'h4', attributes: { 'class': 'bck-title bck-title6'} },
+				{ name: 'Título 7', element: 'h4', attributes: { 'class': 'bck-title bck-title7'} },
+				{ name: 'Título 8', element: 'h4', attributes: { 'class': 'bck-title bck-title8'} },
+				{ name: 'Título 9', element: 'h4', attributes: { 'class': 'bck-title bck-title9'} },
+				{ name: 'Título 10', element: 'h4', attributes: { 'class': 'bck-title bck-title10'} },
+
+				{ name: 'Lista ordenada 1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol1' } },
+				{ name: 'Lista ordenada 2', element: 'ol', attributes: { 'class': 'bck-ol bck-ol2' } },
+				{ name: 'Lista ordenada 3', element: 'ol', attributes: { 'class': 'bck-ol bck-ol3' } },
+				{ name: 'Lista ordenada 4', element: 'ol', attributes: { 'class': 'bck-ol bck-ol4' } },
+				{ name: 'Lista ordenada 5', element: 'ol', attributes: { 'class': 'bck-ol bck-ol5' } },
+				{ name: 'Lista ordenada 6', element: 'ol', attributes: { 'class': 'bck-ol bck-ol6' } },
+				{ name: 'Lista ordenada 7', element: 'ol', attributes: { 'class': 'bck-ol bck-ol7' } },
+				{ name: 'Lista ordenada 8', element: 'ol', attributes: { 'class': 'bck-ol bck-ol8' } },
+				{ name: 'Lista ordenada 9', element: 'ol', attributes: { 'class': 'bck-ol bck-ol9' } },
+
+				{ name: 'Lista desordenada 1', element: 'ul', attributes: { 'class': 'bck-ul bck-ul1' } },
+				{ name: 'Lista desordenada 2', element: 'ul', attributes: { 'class': 'bck-ul bck-ul2' } },
+				{ name: 'Lista desordenada 3', element: 'ul', attributes: { 'class': 'bck-ul bck-ul3' } },
+
+				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box1' } },
+				{ name: 'Caja 2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box2' } },
+				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box3' } },
+				{ name: 'Caja 4', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box4' } },
+				{ name: 'Caja 5', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box5' } },
+				{ name: 'Caja 6', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box6' } },
+				{ name: 'Caja 7', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box7' } },
+				{ name: 'Caja 8', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box8' } },
+				{ name: 'Caja 9', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box9' } },
+				{ name: 'Caja 10', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box10' } },
+			]
+		},
+
+		init: function (scope) {
+			var that = scope || this;
+			//BK-15873 Utilizamos this.parent declarada al inicio de la clase
+			this.parent.init.call(that);
+			that.addActivityTitle();
+			if(window.esWeb) return;
+			that.removeFinalSlide();
+			that.handleScrollEnd();
+			that.setTooltip();
+			window.editar && that.configEditor();
+
+			if ($('.navbar-bottom').length > 0) {
+ 				$('.navbar-bottom ol').wrapAll('<div id="bottom-navigator"></div>');
+		 		var width = 0;
+		 		$('.navbar-bottom li').each(function(i, elem){ width += $(elem).outerWidth(true); });
+		 		$('.navbar-bottom ol').css('width', width * 1.1);
+		 		var scroll = new IScroll('#bottom-navigator', {
+		 			scrollX: true,
+		 			scrollY: false,
+		 			eventPassthrough: true
+		 		});
+		 		scroll.on('scrollEnd', that.handleScrollEnd);
+		 		that.handleScrollEnd.call(scroll);
+	 		}
+
+		},
+
+		configEditor: function (editor) {
+			editor.dtd.$removeEmpty['span'] = false;
+		},
+
+
+		addActivityTitle: function () {
+			if (!blink.courseInfo || !blink.courseInfo.unit) return;
+			$('.libro-left').find('.title').html(function () {
+				return $(this).html().trim() + ' > ' + blink.courseInfo.unit;
+			});
+		},
+
+		handleScrollEnd: function () {
+ 			$('#bottom-navigator')
+ 				.removeClass('show_left')
+ 				.removeClass('show_right');
+
+ 			if (this.x < 0) {
+ 				$('#bottom-navigator').addClass('show_left');
+ 			}
+ 			if (this.x > this.maxScrollX) {
+ 				$('#bottom-navigator').addClass('show_right');
+ 			}
+
+ 		},
+
+		setTooltip: function () {},
+
+		//BK-15873 Quitamos la funcion getEditorStyles para que la herede de basic
+	};
+
+	smchile2021Style.prototype = _.extend({}, new blink.theme.styles.basic(), smchile2021Style.prototype);
+
+	blink.theme.styles.smchile2021 = smchile2021Style;
+
+})( blink );
